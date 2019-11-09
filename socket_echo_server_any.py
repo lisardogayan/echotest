3f2fdf26f8d06c2336aa1638af51063f89553e1a
lisardogayan <lisardogayan@gmail.com>
2019-11-09 23:32:26 +0000

    Initial echo files

---
+++ socket_echo_server_any.py
@@ -0,0 +1,31 @@
+# socket_echo_server_any.py
+# Referencia:
+# https://pymotw.com/3/socket/tcp.html
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+server_address = ('', 10000)
+sock.bind(server_address)
+print('starting up on {} port {}'.format(*sock.getsockname()))
+sock.listen(1)
+
+while True:
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    try:
+        print('client connected:', client_address)
+        while True:
+            data = connection.recv(16)
+            print('received {!r}'.format(data))
+            if data:
+                connection.sendall(data)
+            else:
+                break
+    finally:
+        connection.close()
+
